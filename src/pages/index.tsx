af1db603d1c640c159d5e4509525c1d4e8df30b0
matheuz233 <matheusaugus233@gmail.com>
2025-07-22 23:26:24 -0300

    feat: :hammer: Instala o ContentLayer como dependência do projeto e adiciona primeiro post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { LadingPage } from "@/templates/lading-page/lading-page"
 
+import { allPosts } from 'contentlayer/generated';
+
 export default function Home() {
+  console.debug(allPosts);
   return <LadingPage />
 }
