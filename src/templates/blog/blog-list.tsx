eec0ce249fbd638c9d01084c3fecb476278a74bb
matheuz233 <matheusaugus233@gmail.com>
2025-07-22 23:53:14 -0300

    feat: :hammer: Adiciona componente PostCard e integra na lista de blogs

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,3 +1,4 @@
+import { PostCard } from "@/components/post-card/post-card"
 import { Search } from "@/components/search/search"
 
 export function BlogList() {
@@ -6,22 +7,28 @@ export function BlogList() {
       <header className="">
         <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
           <div className="flex flex-col gap-4 md:px-0">
-            {/* TAG */}
             <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
               BLOG
             </span>
-
-            {/* Titulo */}
             <h1 className="text-balance text-start md:text-left text-heading-lg md:text-heading-xl max-w-2xl text-gray-100">
               Dicas e estratégias para impulsionar seu negócio
             </h1>
           </div>
-          {/* Search */}
           <Search />
         </div>
       </header>
 
-      {/* Listagem de posts */}
+      <PostCard
+        title="Transformando seu negócio em uma loja virtual"
+        description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online..."
+        date="20/12/2024"
+        slug="transformando"
+        image="/assets/primeiro-post.png"
+        author={{
+          avatar: "/customer-01.png",
+          name: "Aspen Dokidis",
+        }}
+      />
     </div>
   )
 }
