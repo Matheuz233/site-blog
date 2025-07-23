73290c4c834b551b36b5a84b679005e6031fefd6
matheuz233 <matheusaugus233@gmail.com>
2025-07-22 23:47:36 -0300

    feat: :hammer: Adiciona componente de busca e integra na lista de blogs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import { LadingPage } from "@/templates/lading-page/lading-page"
 
-import { allPosts } from 'contentlayer/generated';
-
 export default function Home() {
-  console.debug(allPosts);
   return <LadingPage />
 }
