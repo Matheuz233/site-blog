ea44c5425b6bb23ff92c35eb07e854483cada2a0
matheuz233 <matheusaugus233@gmail.com>
2025-07-28 22:29:24 -0300

    feat: :hammer: Adiciona suporte a autores nos posts e implementa componente PostGridCard

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,10 +1,28 @@
 import { PostCard } from "@/components/post-card/post-card"
+import { PostGridCard } from "@/components/post-grid-card/post-grid-card"
 import { Search } from "@/components/search/search"
+import { allPosts } from "contentlayer/generated"
+import { Inbox } from "lucide-react"
+import { useRouter } from "next/router"
 
 export function BlogList() {
+  const router = useRouter()
+  const query = router.query.q as string
+  const pageTitle = query
+    ? `Resultados de busca para "${query}"`
+    : "Dicas e estratégias para impulsionar seu negócio"
+
+  const posts = query
+    ? allPosts.filter((post) =>
+        post.title.toLocaleLowerCase()?.includes(query.toLocaleLowerCase())
+      )
+    : allPosts
+
+  const hasPosts = posts?.length > 0
+
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
-      <header className="">
+      <header className="pb-14">
         <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
           <div className="flex flex-col gap-4 md:px-0">
             <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
@@ -18,17 +36,34 @@ export function BlogList() {
         </div>
       </header>
 
-      <PostCard
-        title="Transformando seu negócio em uma loja virtual"
-        description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online..."
-        date="20/12/2024"
-        slug="transformando"
-        image="/assets/primeiro-post.png"
-        author={{
-          avatar: "/customer-01.png",
-          name: "Aspen Dokidis",
-        }}
-      />
+      {hasPosts && (
+        <PostGridCard>
+          {posts.map((post) => (
+            <PostCard
+              key={post._id}
+              title={post.title}
+              description={post.description}
+              date={new Date(post.date).toLocaleDateString("pt-BR")}
+              slug={post.slug}
+              image={post.image}
+              author={{
+                avatar: post.author.avatar,
+                name: post.author.name,
+              }}
+            />
+          ))}
+        </PostGridCard>
+      )}
+
+      {!hasPosts && (
+        <div className="container px-8">
+          <div className="flex flex-col items-center justify-center gap-8 border-dashed border-2 border-gray-300 p-8 md:p-12 rounded-lg">
+            <Inbox className="h-12 w-12 text-cyan-100" />
+
+            <p className="text-gray-100 text-center">Nenhum post encontrado.</p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
